a40371a4a4577778c7a168d3030ae9e356bc1750
Ranjit Jhala <jhala@cs.ucsd.edu>
2020-07-05 16:33:00 -0700

    front & back OK with rendering ALL student classes

--- client/src/services/api.ts
+++ client/src/services/api.ts
@@ -8,8 +8,8 @@ interface ApiService {
   isSignedIn(): boolean;
 }
 
-// const module: ApiService = Server;
-const module: ApiService = Mock;
+const module: ApiService = Server;
+// const module: ApiService = Mock;
 
 // if (process.env.VUE_APP_MOCK_API_SERVICE == "true") {
 // if (0 == 1) {
